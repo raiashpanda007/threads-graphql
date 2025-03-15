c2cb501b92779c0592537629ca704a4e5d1e872d
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-03-15 13:04:38 +0530

    refactor: update project name, add GraphQL server, and enhance package dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,34 @@
 import express from 'express'
+import { ApolloServer } from '@apollo/server'
+import { expressMiddleware } from '@apollo/server/express4'
 
-const app = express()
+async function StartServer() {
+
+    const app = express()
+    const graphqlServer = new ApolloServer({
+        typeDefs: `
+            type Query {
+                hello: String
+            }
+        `,
+        resolvers: {
+            Query:{
+                hello: () => 'Hello World!'
+            }
+        },
+    });
+    app.use(express.json())
+    await graphqlServer.start()
 
 
-app.use(express.json())
+    app.use('/graphql', expressMiddleware(graphqlServer))
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
+    app.listen(3000, () => {
+        console.log('Server is running on http://localhost:3000')
+    }
+    )
+}
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+StartServer()
